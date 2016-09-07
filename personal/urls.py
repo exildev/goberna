c11bcd84433b84a689d65b3447eeb38eafce0eb2
luismoralesp <luismiguel.mopa@gmail.com>
2016-09-06 19:20:00 -0500

    json

--- personal/urls.py
+++ personal/urls.py
@@ -3,4 +3,6 @@ import views
 
 urlpatterns = [
     url(r'^login/', views.Login.as_view()),
+    url(r'^departamento/list/$', views.DepartamentoListView.as_view(), ),
+    url(r'^persona/list/$', views.PersonaListView.as_view(), ),
 ]
