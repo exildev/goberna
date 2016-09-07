1b8daa041548dfb1a01e87c7b7525ae0746e8d68
mariobp <mariobarrpach@gmail.com>
2016-09-07 17:08:26 -0500

    Login de ciudadano

--- ciudadanos/urls.py
+++ ciudadanos/urls.py
@@ -3,6 +3,7 @@ import views
 
 urlpatterns = [
     url(r'^login/$', views.LoginSupra.as_view(), name="login"),
+    url(r'^login/movil/$', views.LoginMovil.as_view(), name="loginmovil"),
     url(r'^logout/$', views.logoutCiudadano, name="login"),
     url(r'^registro/$', views.RegistroSupra.as_view(), name="login"),
 ]
