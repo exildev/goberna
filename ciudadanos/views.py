a2cbcd10d72f10209833fe34a050b774f9f0a2a5
mariobp <mariobarrpach@gmail.com>
2016-09-06 21:14:35 -0500

    login

--- ciudadanos/views.py
+++ ciudadanos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from supra import views as supra
 import forms
 import models
@@ -10,6 +10,24 @@ supra.SupraConf.ACCECC_CONTROL["allow"] = True
 class LoginSupra(supra.SupraSession):
     model = models.Ciudadano
     template_name = "ciudadanos/login.html"
+
+    def form_valid(self, form):
+        instance = form.save()
+        for inline in self.validated_inilines:
+            inline.instance = instance
+            inline.save()
+        # end for
+        return HttpResponseRedirect('/')
+    # end def
+
+    def form_invalid(self, form):
+        errors = dict(form.errors)
+        print errors
+        for i in self.invalided_inilines:
+			errors['inlines'] = list(i.errors)
+		# end for
+        return render(self.request, self.template_name, {"form": form})
+    # end def
 # end class
 
 
