4d8271589ae17f3d701afac4d86fbdab5e492366
mariobp <mariobarrpach@gmail.com>
2016-09-07 01:10:01 -0500

    Servicios

--- servicios/urls.py
+++ servicios/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     url(r'^registro/pro/no/$', views.RegistroProNSupra.as_view(), name="registropron"),
     url(r'^registro/tarjeta/pro/$', views.TarjetaSupra.as_view(), name="tarjetapro"),
     url(r'^registro/pasaporte/$', views.PasaporteSupra.as_view(), name="pasaporte"),
+    url(r'^list/pro/si/$', views.RegistroProSList.as_view(), name="registroproslist"),
+    url(r'^list/pro/no/$', views.RegistroProNList.as_view(), name="registropronlist"),
+    url(r'^list/tarjeta/pro/$', views.TarjetaProList.as_view(), name="tarjetaprolist"),
+    url(r'^list/pasaporte/$', views.PasaporteList.as_view(), name="pasaportelist"),
 ]
