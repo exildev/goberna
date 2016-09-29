9741be8ec65736f0b7f44cc0206849532b02f2c0
mariobp <mariobarrpach@gmail.com>
2016-09-29 12:30:07 -0500

    Static and Media

--- goberna/settings.py
+++ goberna/settings.py
@@ -295,7 +295,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/goberna/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/goberna/media/'
